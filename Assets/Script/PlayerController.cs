a87a9dc4d3e8a08bf9ab2fc03d981bc8aa70caad
Wuming77 <2313432829@qq.com>
2021-07-24 11:12:15 +0800

    第一次提交

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 10.0f;
+
+    public GameObject projectilePerfab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        MoveRightOrLeft();
+        MoveForwardOrBack();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePerfab, new Vector3(transform.position.x,
+                2.5f, transform.position.z), projectilePerfab.transform.rotation);
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+    void MoveRightOrLeft()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        if (transform.position.x <= -20 && horizontalInput > 0) //到达左边界只能向右移动
+        {
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        }
+        if (transform.position.x >= 20 && horizontalInput < 0)  //到达右边界只能向左移动
+        {
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        }
+        //horizontalInput = Input.GetAxis("Horizontal");
+        if (transform.position.x < 20 && transform.position.x > -20)
+        {
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        }       
+    }
+    void MoveForwardOrBack()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        if (transform.position.z <= -7.4 && verticalInput > 0) //到达下边界只能向前移动
+        {
+            transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+        }
+        if (transform.position.z >= 20 && verticalInput < 0)  //到达上边界只能向后移动
+        {
+            transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+        }
+        //horizontalInput = Input.GetAxis("verticalInput");
+        if (transform.position.z < 20 && transform.position.z > -7.4f)
+        {
+            transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+        }
+    }
+}
---
+++ Assets/Script/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPerfabs;
+
+    private float spawnRangeX = 20;
+    private float spawnRangeZ = 30;
+
+    private float startDelay = 3;
+    private float spawnInterval = 2.0f;
+    //public int animalIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
+        int animalIndex = Random.Range(0, animalPerfabs.Length);
+        Instantiate(animalPerfabs[animalIndex], spawnPos,
+            animalPerfabs[animalIndex].transform.rotation);
+    }
+}
